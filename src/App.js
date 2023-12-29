396ad8cb63e1a9021005b835804b42f6caafc24d
Richa Gupta <richagupta8922@gmail.com>
2023-12-29 04:47:26 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 
 function App() {
    const [result,setResult]=useState('')
+   function handle(e){
+    if(e.target.value === '.' && result.includes('.')) return;
+    setResult(result +e.target.value) 
+   }
+   function calculate(){
+    try{
+      setResult(eval(result).toString())
+    }
+    catch{
+      setResult("Error");
+    }
+   
+
+   } 
+   
+   
   return (
     <>
       <div className='container'>
@@ -12,32 +28,32 @@ function App() {
             <input type="text" value={result}/>
           </div>
           <div>
-            <input type="button"  value="7" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button"  value="8" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button"  value="9" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button"  value="/" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button"  value="7" onClick={handle}/>
+            <input type="button"  value="8" onClick={handle}/>
+            <input type="button"  value="9" onClick={handle}/>
+            <input type="button"  value="/" onClick={handle}/>
             </div>
             <div>
-            <input type="button" value="4" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="5" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="6" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="*" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="4" onClick={handle}/>
+            <input type="button" value="5" onClick={handle}/>
+            <input type="button" value="6" onClick={handle}/>
+            <input type="button" value="*" onClick={handle}/>
             </div>
             <div>
-            <input type="button" value="1" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="2" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="3" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="+" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="1" onClick={handle}/>
+            <input type="button" value="2" onClick={handle}/>
+            <input type="button" value="3" onClick={handle}/>
+            <input type="button" value="+" onClick={handle}/>
             </div>
             <div>
-            <input type="button" value="0" onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="." onClick={e=>setResult(result +e.target.value)}/>
-            <input type="button" value="=" onClick={e=>setResult(eval(result))}/>
-            <input type="button" value="-" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="0" onClick={handle}/>
+            <input type="button" value="." onClick={handle}/>
+            <input type="button" value="=" onClick={calculate}/>
+            <input type="button" value="-" onClick={handle}/>
             </div>
             <div className="clear">
             <input type="button" value="clear"  onClick={e=>setResult('')}/> 
-            <input type="button" value="Del"  onClick={e=>setResult(result.slice(0,-1))}/>
+            <input type="button" value="Del"  onClick={e=>setResult(String(result).slice(0,-1))}/>
             </div>
         
           
