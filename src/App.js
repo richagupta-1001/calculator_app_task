f5f3d720ae3a8ab1cd116359d962f4fe82cc34be
richagupta-1001 <richagupta8922@gmail.com>
2023-12-28 23:47:47 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+   const [result,setResult]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='calculator'>
+          <form action="">
+          <div className="result">
+            <input type="text" value={result}/>
+          </div>
+          <div>
+            <input type="button"  value="7" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button"  value="8" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button"  value="9" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button"  value="/" onClick={e=>setResult(result +e.target.value)}/>
+            </div>
+            <div>
+            <input type="button" value="4" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="5" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="6" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="*" onClick={e=>setResult(result +e.target.value)}/>
+            </div>
+            <div>
+            <input type="button" value="1" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="2" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="3" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="+" onClick={e=>setResult(result +e.target.value)}/>
+            </div>
+            <div>
+            <input type="button" value="0" onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="." onClick={e=>setResult(result +e.target.value)}/>
+            <input type="button" value="=" onClick={e=>setResult(eval(result))}/>
+            <input type="button" value="-" onClick={e=>setResult(result +e.target.value)}/>
+            </div>
+            <div className="clear">
+            <input type="button" value="clear"  onClick={e=>setResult('')}/> 
+            <input type="button" value="Del"  onClick={e=>setResult(result.slice(0,-1))}/>
+            </div>
+        
+          
+
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
